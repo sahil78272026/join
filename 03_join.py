1149903f3f2a6abb6e4ab833dabb637f501b51d3
sahil78272026 <sahil.garg78272026@gmail.com>
2022-05-03 21:52:43 +0530

    Add files via upload

---
+++ 03_join.py
@@ -0,0 +1,8 @@
+l = ["Camera", "Laptop", "Phone", "ipad", "Hard Disk", "Nvidia Graphic 3080 card"]
+
+# sentence = "~~".join(l)
+# sentence = "==".join(l)
+sentence = "\n".join(l) # joins the mentioned string and return an string
+# print(l)
+print(sentence)
+print(type(sentence))
